9434f0bab6525b16cc9d6c39f53cbf633d4e3b88
supermaxqaz <maxwell.vanwalsen@mymail.champlain.edu>
2020-02-21 16:52:12 -0500

    Here is the basis of the first assignment

--- GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
+++ GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
@@ -1,7 +1,10 @@
 // GPRO-FW-Launcher.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#include <stdio.h>
 
 int main()
 {
+	int test = foo(9000);
 
+	printf("\n %d \n", test);
 }
